864b5c1c73a6a453761218503945560df2015551
danijeldz <danijeldzankic@hotmail.com>
2020-03-11 23:34:30 +0100

    initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      title: "My Recipes"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
